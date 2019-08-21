c89fb6cceb834546b1a3246490afb22ccffce2a6
AnilChinchawale <anil24593@gmail.com>
2019-08-21 15:36:51 +0530

    Alert issue fixed.

--- src/app.js
+++ src/app.js
@@ -199,14 +199,14 @@ async function run () {
     modalDialogCustom.alert('This UNSTABLE ALPHA branch of Remix has been moved to http://ethereum.github.io/remix-live-alpha.')
   } else if (window.location.hostname === 'remix-alpha.ethereum.org' ||
   (window.location.hostname === 'ethereum.github.io' && window.location.pathname.indexOf('/remix-live-alpha') === 0)) {
-    modalDialogCustom.alert(`Welcome to the Remix alpha instance. Please use it to try out latest features. But use preferably https://remix.ethereum.org for any production work.`)
+    modalDialogCustom.alert(`Welcome to the Remix alpha instance. Please use it to try out latest features. But use preferably https://remix.xinfin.network for any production work.`)
   } else if (window.location.protocol.indexOf('http') === 0 &&
-  window.location.hostname !== 'remix.ethereum.org' &&
+  window.location.hostname !== 'remix.xinfin.network' &&
   window.location.hostname !== 'localhost' &&
   window.location.hostname !== '127.0.0.1') {
-    modalDialogCustom.alert(`The Remix IDE has moved to http://remix.ethereum.org.\n
+    modalDialogCustom.alert(`The Remix IDE has moved to http://remix.xinfin.network.\n
 This instance of Remix you are visiting WILL NOT BE UPDATED.\n
-Please make a backup of your contracts and start using http://remix.ethereum.org`)
+Please make a backup of your contracts and start using http://remix.xinfin.network`)
   }
   if (window.location.protocol.indexOf('https') === 0) {
     toolTip('You are using an `https` connection. Please switch to `http` if you are using Remix against an `http Web3 provider` or allow Mixed Content in your browser.')
